8423f9b4f30e1f1c5446ff2668de8203f403812f
Jack <soarn@soarn.pro>
2023-05-06 11:56:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function main() {
 
   const lines = [];
   for(let i = 0; i < numArtitst; i++) {
-    const plays = json.topartists.artist[i].playcount;
+    const plays = json.topartists.artist.playcount;
     let name =  json.topartists.artist[i].name.substring(0, 25);
     // trim off long widechars
     for(let i = 24; i >= 0; i--) {
